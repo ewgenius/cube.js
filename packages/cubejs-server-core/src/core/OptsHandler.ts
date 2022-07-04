781801246a159059e7d2c73e238fa9b6030588c7
buntarb <buntarb@gmail.com>
2022-07-04 23:26:38 +0300

    implementation draft

--- packages/cubejs-server-core/src/core/OptsHandler.ts
+++ packages/cubejs-server-core/src/core/OptsHandler.ts
@@ -600,7 +600,10 @@ export class OptsHandler {
           typeof this.initializedOptions.scheduledRefreshTimer === 'boolean' &&
           this.initializedOptions.scheduledRefreshTimer
         ) ||
-        typeof this.initializedOptions.scheduledRefreshTimer === 'number'
+        (
+          typeof this.initializedOptions.scheduledRefreshTimer === 'number' &&
+          this.initializedOptions.scheduledRefreshTimer !== 0
+        )
       )
     );
   }
@@ -623,8 +626,8 @@ export class OptsHandler {
   }
 
   /**
-   * Decorate `OrchestratorOptions` with `queueOptions` property which include
-   * concurrency calculation logic.
+   * Returns `OrchestratorInitedOptions` based on provided `OrchestratorOptions`
+   * and request context.
    */
   public getOrchestratorInitializedOptions(
     context: RequestContext,
@@ -633,8 +636,8 @@ export class OptsHandler {
     const clone = cloneDeep(orchestratorOptions);
 
     // rollup only mode (querying pre-aggs only)
-    clone.rollupOnlyMode = orchestratorOptions.rollupOnlyMode !== undefined
-      ? orchestratorOptions.rollupOnlyMode
+    clone.rollupOnlyMode = clone.rollupOnlyMode !== undefined
+      ? clone.rollupOnlyMode
       : getEnv('rollupOnlyMode');
 
     // query queue options
@@ -652,9 +655,12 @@ export class OptsHandler {
     );
 
     // pre-aggs external refresh flag (force to run pre-aggs build flow first if
-    // pre-agg is not exists/updated at the query moment).
+    // pre-agg is not exists/updated at the query moment). Initially the default
+    // was equal to [rollupOnlyMode && !scheduledRefreshTimer].
     clone.preAggregationsOptions.externalRefresh =
-      clone.rollupOnlyMode && !this.initializedOptions.scheduledRefreshTimer;
+      clone.preAggregationsOptions.externalRefresh !== undefined
+        ? clone.preAggregationsOptions.externalRefresh
+        : !this.configuredAsPreAggsBuilder();
 
     return clone;
   }
